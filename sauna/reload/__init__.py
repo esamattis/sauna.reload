13db508b5d19e3c3ce8f6ba71ffc38cac8dfe3aa
Asko Soukka <asko.soukka@iki.fi>
2011-07-27 23:52:54 +0300

    Oops. Restored .

--- sauna/reload/__init__.py
+++ sauna/reload/__init__.py
@@ -9,11 +9,6 @@
 # recipe = plone.recipe.zope2instance
 # zope-conf-additional = %import sauna.reload
 
-from reload import forkloop
-
-def initialize(context):
-    """Initializer called when used as a Zope 2 product."""
-
 import sys
 
 from sauna.reload.monkeypatcher import MonkeyPatchingLoader
