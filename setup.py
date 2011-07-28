5f575c39379cd5a89a10b15681d56644eea110aa
Mikko <mikko@mfabrik.com>
2011-07-28 18:30:38 +0300

    Reflect README changes in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0'
+version = '0.1'
 
 setup(name='sauna.reload',
       version=version,
       description="",
-      long_description=open("README.markdown").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
@@ -14,7 +14,7 @@ setup(name='sauna.reload',
         "Framework :: Plone",
         "Programming Language :: Python",
         ],
-      keywords='plone reload zope2',
+      keywords='plone reload zope2 restart auto',
       author='Esa-Matti Suuronen',
       author_email='esa-matti@suuronen.org',
       url='http://github.com/epeli/sauna.reload',
