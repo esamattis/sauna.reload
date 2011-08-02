5a23da5db2819cbc2c568fb0c1ba1cc7923edb22
Esa-Matti Suuronen <esa-matti@suuronen.org>
2011-08-02 12:26:45 +0300

    Wrap db-actions to locks just to be safe.

--- sauna/reload/db.py
+++ sauna/reload/db.py
@@ -11,23 +11,32 @@ class FileStorageIndex(object):
         self.storage = storage
 
     def save(self):
-        # Save ``Data.fs.index`` before dying to notify the next child of the
-        # peristent changes
-        self.storage._save_index()
+
+         # Save ``Data.fs.index`` before dying to notify the next child of the
+         # peristent changes
+         self.storage._lock_acquire()
+         try:
+             self.storage._save_index()
+         finally:
+             self.storage._lock_release()
+
         # TODO: The code aboce should be abstracted with ZCA to make
         # ``sauna.reload`` to be able to support also other storages than ZODB.
 
     def restore(self):
-        # Load saved ``Data.fs.index`` to see the persistent changes created by
-        # the previous child.
-        index, start, ltid = self.storage._restore_index()
-        # Sanity check. Last transaction in restored index must match
-        # the last transaction given by FileStorage transaction iterator.
-        if ltid == tuple(self.storage.iterator())[-1].tid:
-            self.storage._initIndex(index, {})
-            self.storage._pos, self.storage._oid, tid = read_index(
-                self.storage._file, self.storage._file_name, index, {},
-                stop=None, ltid=ltid, start=start, read_only=False)
-            self.storage._ltid = tid
-            self.storage._ts = tid = TimeStamp(tid)
-
+         self.storage._lock_acquire()
+         try:
+            # Load saved ``Data.fs.index`` to see the persistent changes created by
+            # the previous child.
+            index, start, ltid = self.storage._restore_index()
+            # Sanity check. Last transaction in restored index must match
+            # the last transaction given by FileStorage transaction iterator.
+            if ltid == tuple(self.storage.iterator())[-1].tid:
+                self.storage._initIndex(index, {})
+                self.storage._pos, self.storage._oid, tid = read_index(
+                    self.storage._file, self.storage._file_name, index, {},
+                    stop=None, ltid=ltid, start=start, read_only=False)
+                self.storage._ltid = tid
+                self.storage._ts = tid = TimeStamp(tid)
+         finally:
+             self.storage._lock_release()
