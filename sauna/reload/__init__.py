65102632f4bf408eab2af950c3a8b62a0ee00e79
Asko Soukka <asko.soukka@iki.fi>
2011-08-15 11:11:34 +0300

    Re-licensed to ZPL 2.1.

--- sauna/reload/__init__.py
+++ sauna/reload/__init__.py
@@ -1,24 +1,18 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2011 University of Jyväskylä
+# Copyright (c) 2011 University of Jyväskylä and Contributors.
+#
+# All Rights Reserved.
 #
 # Authors:
 #     Esa-Matti Suuronen <esa-matti@suuronen.org>
 #     Asko Soukka <asko.soukka@iki.fi>
 #
-# This file is part of sauna.reload.
-#
-# sauna.reload is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# sauna.reload is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with sauna.reload.  If not, see <http://www.gnu.org/licenses/>.
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
 
 """sauna.reload"""
 
