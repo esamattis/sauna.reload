f94aeaa07e8eeced9931cdda25421bc0b701f25b
Asko Soukka <asko.soukka@iki.fi>
2012-04-21 10:07:19 +0300

    Fixed p.a.theming's plugins to work with sauna.reload. Fixes #15.

---
+++ sauna/reload/plugins/theming.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""Configures p.a.theming-plugins for *sauna.reload*-controlled products"""
+
+from plone.resource.utils import iterDirectoriesOfType
+
+from plone.app.theming.interfaces import THEME_RESOURCE_NAME
+
+from plone.app.theming.plugins.utils import getPlugins
+from plone.app.theming.plugins.utils import getPluginSettings
+
+from sauna.reload import reload_paths
+
+
+def onStartup(event):
+    """Call onDiscovery() on each plugin for each theme on startup"""
+
+    plugins = getPlugins()
+
+    for themeDirectory in iterDirectoriesOfType(THEME_RESOURCE_NAME):
+        if themeDirectory.directory in reload_paths:  # only for sauna.reload!
+            pluginSettings = getPluginSettings(themeDirectory, plugins)
+
+            for name, plugin in plugins:
+                plugin.onDiscovery(themeDirectory.__name__,
+                                   pluginSettings[name],
+                                   pluginSettings)
